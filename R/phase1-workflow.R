c920b78bf85053d70b824cf8ca7cf13fe2177ba8
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-12-25 02:31:52 -0200

    initial version of the workflow phase1

---
+++ R/phase1-workflow.R
@@ -0,0 +1,117 @@
+#!/usr/bin/Rscript
+
+source("phase1.R");
+
+#
+# This script gets a directory with CSV files from StarPU PajeNGR framework 
+# and process them using the_reader_function. As output, it generates
+# ready-to-plot feather files.
+#
+
+##############################
+# Usage                      #
+##############################
+usage <- function ()
+{
+    stop("Usage: pre-workflow.R <directory> <application>\n   where <directory> contains CSV files of the workflow;\n   where <application> is either cholesky or qrmumps.", call.=FALSE)
+}
+
+# Get the arguments to this script
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) < 2) {
+    usage();
+}
+
+input.directory = args[[1]];
+input.application = args[[2]];
+
+if (is.null(input.directory) ||
+    (input.application != "cholesky" && input.application != "qrmumps")){
+    usage();
+}
+
+if (input.application == "cholesky"){
+    states.fun = cholesky_colors;
+    states.filter.strict = FALSE;
+}else if (input.application == "qrmumps") {
+    states.fun = qrmumps_colors;
+    states.filter.strict = FALSE;
+}else if (input.application == "cholesky_pastix") {
+    states.fun = cholesky_pastix_colors;
+    states.filter.strict = FALSE;
+}
+
+setwd(input.directory);
+
+data <- the_reader_function (directory = input.directory,
+                             app_states_fun = states.fun,
+                             strict_state_filter = states.filter.strict,
+                             whichApplication = input.application);
+
+loginfo("Let's start to write the pre-processed files as feather data");
+
+# State
+filename <- "pre.state.feather";
+loginfo(filename);
+if (!is.null(data$State)){
+    write_feather(data$State, filename);
+}else{
+    loginfo(paste("Data for", filename, "has not been feathered because is empty."));
+}
+
+# Variable
+filename <- "pre.variable.feather";
+loginfo(filename);
+if (!is.null(data$Variable)){
+    write_feather(data$Variable, filename);
+}else{
+    loginfo(paste("Data for", filename, "has not been feathered because is empty."));
+}
+
+# Link
+filename <- "pre.link.feather";
+loginfo(filename);
+if (!is.null(data$Link)){
+    write_feather(data$Link, filename);
+}else{
+    loginfo(paste("Data for", filename, "has not been feathered because is empty."));
+}
+
+# DAG
+filename <- "pre.dag.feather";
+loginfo(filename);
+if (!is.null(data$DAG)){
+    write_feather(data$DAG, filename);
+}else{
+    loginfo(paste("Data for", filename, "has not been feathered because is empty."));
+}
+
+# Y
+filename <- "pre.y.feather";
+loginfo(filename);
+if (!is.null(data$Y)){
+    write_feather(data$Y, filename);
+}else{
+    loginfo(paste("Data for", filename, "has not been feathered because is empty."));
+}
+
+# ATree
+filename <- "pre.atree.feather";
+loginfo(filename);
+if (!is.null(data$ATree)){
+    write_feather(data$ATree, filename);
+}else{
+    loginfo(paste("Data for", filename, "has not been feathered because is empty."));
+}
+
+# Gaps
+filename <- "pre.gaps.feather";
+loginfo(filename);
+if (!is.null(data$Gaps)){
+    write_feather(data$Gaps, filename);
+}else{
+    loginfo(paste("Data for", filename, "has not been feathered because is empty."));
+}
+
+loginfo("Pre-process finished correctly.");

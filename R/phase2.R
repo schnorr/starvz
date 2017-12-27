48b2a6acb3637d7ece31f5862ded7c111e3740ca
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-12-27 01:59:28 -0200

    remove reduntant file

--- R/phase2.R
+++
@@ -1,9 +0,0 @@
-# see:
-# https://stackoverflow.com/questions/1815606/rscript-determine-path-of-the-executing-script (Suppressingfire answer)
-initial.options <- commandArgs(trailingOnly = FALSE)
-file.arg.name <- "--file="
-script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
-script.basename <- dirname(script.name)
-other.name <- paste(sep="/", script.basename, "deps.R")
-print(paste("Sourcing",other.name,"from",script.name))
-source(other.name)

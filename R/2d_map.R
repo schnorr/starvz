cfb9603e229594a38f35f51cb0c483dbc8014820
Lucas Nesi <lucas.nesi@inf.ufrgs.br>
2020-02-07 13:02:40 -0300

    Correct 2d map

--- R/2d_map.R
+++ R/2d_map.R
@@ -16,10 +16,10 @@ data <- the_fast_reader_function(directory);
 
 print("Data has been read");
 
-x <- data$data_handle %>% filter(HomeNode==0) %>%
+x <- data$Data_handle %>% filter(HomeNode==0) %>%
                      separate(Coordinates, c("Y", "X")) %>%
                      mutate(X=as.numeric(X), Y=as.numeric(Y)) %>%
                      ggplot(aes(x=X, y=Y, fill=factor(MPIRank))) +
-                     geom_tile() 
-                     
+                     geom_tile()
+
 ggsave(name, x, width=10, height=10)

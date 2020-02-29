30c47182f9736f56a5d6eb38ec0b9e8b915d4a43
Lucas Nesi <lucas31nesi@hotmail.com>
2020-02-29 09:06:58 -0300

    change how 2d_map works

--- R/2d_map.R
+++ R/2d_map.R
@@ -16,10 +16,10 @@ data <- the_fast_reader_function(directory);
 
 print("Data has been read");
 
-x <- data$Data_handle %>% filter(HomeNode==0) %>%
+x <- data$Data_handle %>% select(MPIOwner, Coordinates) %>% unique() %>%
                      separate(Coordinates, c("Y", "X")) %>%
                      mutate(X=as.numeric(X), Y=as.numeric(Y)) %>%
-                     ggplot(aes(x=X, y=Y, fill=factor(MPIRank))) +
+                     ggplot(aes(x=X, y=Y, fill=factor(MPIOwner))) +
                      geom_tile()
 
 ggsave(name, x, width=10, height=10)

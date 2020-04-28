1b665b04d43629606ba5fa76087c0204949e247b
Lucas Nesi <lucas31nesi@hotmail.com>
2020-04-28 16:06:26 -0300

    add convert feather2parquet function

--- R_package/R/write_functions.R
+++ R_package/R/write_functions.R
@@ -1,8 +1,9 @@
-starvz_write_feather <- function(data){
+starvz_write_feather <- function(data, directory="."){
   check_arrow();
   invisible(data %>% purrr::list_modify("Origin" = NULL) %>% names %>%
   lapply(function(x) {
-      filename <- paste0(tolower(x), ".feather"); loginfo(filename);
+      filename <- paste0(directory, "/", tolower(x), ".feather");
+      loginfo(filename);
       if (!is.null(data[[x]])){
           if(is.data.frame(data[[x]])){
               write_feather(data[[x]], filename);
@@ -15,11 +16,12 @@ starvz_write_feather <- function(data){
   }))
 }
 
-starvz_write_parquet <- function(data){
+starvz_write_parquet <- function(data, directory="."){
   check_arrow();
   invisible(data %>% purrr::list_modify("Origin" = NULL) %>% names %>%
   lapply(function(x) {
-      filename <- paste0(tolower(x), ".parquet"); loginfo(filename);
+      filename <- paste0(directory, "/", tolower(x), ".parquet");
+      loginfo(filename);
       if (!is.null(data[[x]])){
           if(is.data.frame(data[[x]])){
               properties = ParquetWriterProperties$create(data[[x]], compression = "gzip")
@@ -31,4 +33,9 @@ starvz_write_parquet <- function(data){
           loginfo(paste("Data for", filename, "has not been feathered because is empty."));
       }
   }))
+}
+
+convert_feather_parquet <- function(directory = "."){
+    data <- starvz_read_feather(directory)
+    starvz_write_parquet(data, directory)
 }

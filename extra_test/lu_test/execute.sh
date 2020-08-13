70a6a5f8baa1eeab78ecac894eefbf3f2d7ffdcf
Lucas Nesi <lucas.nesi@inf.ufrgs.br>
2020-08-13 15:42:56 -0300

    return original

--- extra_test/lu_test/execute.sh
+++ extra_test/lu_test/execute.sh
@@ -3,13 +3,13 @@
 TEST="../../extra_test/lu_test/"
 
 cd ../../inst/tools/
-#./phase1-workflow.sh $TEST/fxt/ lu 1
+./phase1-workflow.sh $TEST/fxt/ lu 1
 ./phase2-workflow.R $TEST/fxt/ $TEST/config.yaml $TEST/img.png
-#./phase2-workflow.R $TEST/fxt/ $TEST/min.yaml $TEST/min.png
-#./phase2-workflow.R $TEST/fxt/ $TEST/selected.yaml $TEST/selected.png
-#./phase2-workflow.R $TEST/fxt/ $TEST/agg.yaml $TEST/agg.png
-#./phase2-workflow.R $TEST/fxt/ $TEST/agg_dynamic.yaml $TEST/agg_dynamic.png
-#./phase2-workflow.R $TEST/fxt/ $TEST/agg_static.yaml $TEST/agg_static.png
-#./phase2-workflow.R $TEST/fxt/ $TEST/imb.yaml $TEST/imb.png
-#./phase2-workflow.R $TEST/fxt/ $TEST/pmtool.yaml $TEST/pmtool.png
+./phase2-workflow.R $TEST/fxt/ $TEST/min.yaml $TEST/min.png
+./phase2-workflow.R $TEST/fxt/ $TEST/selected.yaml $TEST/selected.png
+./phase2-workflow.R $TEST/fxt/ $TEST/agg.yaml $TEST/agg.png
+./phase2-workflow.R $TEST/fxt/ $TEST/agg_dynamic.yaml $TEST/agg_dynamic.png
+./phase2-workflow.R $TEST/fxt/ $TEST/agg_static.yaml $TEST/agg_static.png
+./phase2-workflow.R $TEST/fxt/ $TEST/imb.yaml $TEST/imb.png
+./phase2-workflow.R $TEST/fxt/ $TEST/pmtool.yaml $TEST/pmtool.png
 cd $TEST

3d37d830da046f7751bf547caaad82c329cb0fb2
Lucas Nesi <lucas.nesi@inf.ufrgs.br>
2020-09-04 01:45:49 -0300

    split tests (test)

--- tests/testthat/test_lu.R
+++ tests/testthat/test_lu.R
@@ -51,12 +51,6 @@ test_that("starvz_plot works", {
  result <- all.equal(nrow(other_data$Application), nrow(starvz_sample_lu$Application))
  expect_equal(result, TRUE)
 
- other_data <- starvz_phase1(system.file("extdata", "qr_trace", package = "starvz"), qrmumps_colors, state_filter=1, whichApplication="qrmumps")
- other_data <- starvz_read(system.file("extdata", "qr_trace", package = "starvz"), system.file("extdata", "qr.yaml", package = "starvz"))
- pl <- NULL
- pl <- starvz_plot(other_data)
- expect_equal(class(pl),c("patchwork", "gg", "ggplot"))
-
  pl <- NULL
  pl <-  panel_handles(starvz_sample_lu, JobId="0_1")
  expect_equal(class(pl),c("gg", "ggplot"))
@@ -64,4 +58,5 @@ test_that("starvz_plot works", {
  pl <- NULL
  pl <-  panel_memory_snap(starvz_sample_lu, 100, 10)
  expect_equal(class(pl),c("gg", "ggplot"))
+
 })
---
+++ tests/testthat/test_qr.R
@@ -0,0 +1,18 @@
+context("Test with QR")
+library(starvz)
+
+test_that("starvz with QR", {
+
+ skip_on_cran()
+
+ skip_if(system.file("extdata", "qr_trace", package = "starvz")=="")
+
+ other_data <- starvz_phase1(system.file("extdata", "qr_trace", package = "starvz"), qrmumps_colors, state_filter=1, whichApplication="qrmumps")
+ other_data <- starvz_read(system.file("extdata", "qr_trace", package = "starvz"), system.file("extdata", "qr.yaml", package = "starvz"))
+ pl <- NULL
+ pl <- starvz_plot(other_data)
+ expect_equal(class(pl),c("patchwork", "gg", "ggplot"))
+
+
+ 
+})

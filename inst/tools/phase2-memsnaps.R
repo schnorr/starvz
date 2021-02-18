1dc57f6261c65af3b99f5c5717deda86ed3d1d96
Lucas Nesi <lucas.nesi@inf.ufrgs.br>
2021-02-18 20:59:19 -0300

    add option to generate memory snaps at main script

---
+++ inst/tools/phase2-memsnaps.R
@@ -0,0 +1,22 @@
+#!/usr/bin/Rscript
+# Call function memsnaps from start to end with informed steps
+
+library(starvz)
+library(magrittr)
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) < 2) {
+    stop("Usage: phase2-memsnaps.R <directory> <step>", call.=FALSE)
+}
+
+directory = args[[1]];
+step = as.integer(args[[2]]);
+
+path <- paste0(directory, "/snap_")
+
+data <- starvz_read(directory, selective=FALSE);
+end <- data$Application %>% .$End %>% max()
+cat("Generating snaps at:")
+cat(directory)
+cat("\n")
+multiple_snaps(data, 0, end, step, path, scale=8, width=4, height=3)
